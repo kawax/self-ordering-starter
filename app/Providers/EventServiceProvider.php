9c44ff61935a596f73ec04b441cefe29bf0d16a6
kawax <kawaxbiz@gmail.com>
2021-01-31 11:08:01 +0900

    php artisan ordering:install

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Listeners\OrderEntryListener;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Revolution\Ordering\Events\OrderEntry;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,8 +14,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        OrderEntry::class => [
+            OrderEntryListener::class,
         ],
     ];
 
