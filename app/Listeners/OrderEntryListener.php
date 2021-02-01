d6d6a391547a8f4e2d402a7ba759f4f3e3d1e4b1
kawax <kawaxbiz@gmail.com>
2021-02-01 19:28:54 +0900

    add line

--- app/Listeners/OrderEntryListener.php
+++ app/Listeners/OrderEntryListener.php
@@ -2,6 +2,8 @@
 
 namespace App\Listeners;
 
+use App\Notifications\OrderEntryNotification;
+use Illuminate\Support\Facades\Notification;
 use Revolution\Ordering\Events\OrderEntry;
 
 class OrderEntryListener
@@ -26,5 +28,8 @@ class OrderEntryListener
     public function handle(OrderEntry $event)
     {
         info($event->table.' : '.$event->memo, $event->items);
+
+        // Notification::route('line-notify', config('line.notify.personal_access_token'))
+        //             ->notify(new OrderEntryNotification($event->items, $event->table, $event->memo));
     }
 }
---
+++ app/Notifications/OrderEntryNotification.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Arr;
+use Revolution\Line\Notifications\LineNotifyChannel;
+use Revolution\Line\Notifications\LineNotifyMessage;
+
+class OrderEntryNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * @var array
+     */
+    public $items;
+
+    public $table;
+
+    public $memo;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $items
+     * @param $table
+     * @param $memo
+     */
+    public function __construct($items, $table, $memo)
+    {
+        $this->items = $items;
+        $this->table = $table;
+        $this->memo = $memo;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [
+            //'mail',
+            LineNotifyChannel::class,
+        ];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * @param  mixed  $notifiable
+     *
+     * @return LineNotifyMessage
+     */
+    public function toLineNotify($notifiable)
+    {
+        $message = collect([
+            '',
+            '◆テーブル：'.$this->table,
+            '◆メモ：'.$this->memo,
+            '◆合計：'.collect($this->items)->sum('price').'円',
+            '◆注文◆'.PHP_EOL.collect($this->items)
+                ->map(fn($item) => '【'.Arr::get($item, 'name').'】('.Arr::get($item, 'price').'円)')
+                ->implode(PHP_EOL),
+        ])->implode(PHP_EOL.PHP_EOL);
+
+        return LineNotifyMessage::create($message);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
